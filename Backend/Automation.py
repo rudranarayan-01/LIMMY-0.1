c9b5ec21c22520dae0dc8428d9458d0b175a5a4b
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-01-31 09:42:08 +0530

    Automation.py

--- Backend/Automation.py
+++ Backend/Automation.py
@@ -44,3 +44,40 @@ SystemChatBot = [{"role": "system", "content": f"Hello, I'm {os.getenv("Username
 def GoogleSearch(Topic):
     search(Topic) # use pywhatkit search funtion to perform a google search
     return True
+
+# Function to generate content using AI and save it to a file
+def Content(Topic):
+    # Nested function to open a file in Notepad
+    def OpenNotepad(File):
+        default_text_editor = "notepad.exe"
+        subprocess.Popen([default_text_editor,File])
+    
+    # Nested function to generate Content using AI Chatbot
+    def ContentWritterAI(prompt):
+        messages.append({"role":"user", "content":f"{prompt}"})
+        
+        completions = client.chat.completions.create(
+            model="mixtral-8x7b-32768",
+            messages= SystemChatBot + messages,
+            max_tokens= 2048,
+            temperature=0.7,
+            top_p=1,
+            stream=True,
+            stop=None
+        )
+        
+        Answer = ""
+        # Process streamed response chunks
+        for chunk in completions:
+            if chunk.choices[0].delta.content:
+                Answer += chunk.choices[0].delta.content
+        
+        Answer = Answer.replace("</s>","")
+        messages.append({"role":"assistant", "content": Answer})
+        return Answer
+    
+    Topic: str = Topic.replace("Content", "")
+    ContentByAI = ContentWritterAI(Topic)
+    
+    # Save the generated content to a file
+    
