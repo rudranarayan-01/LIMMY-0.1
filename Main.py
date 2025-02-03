b40c37edc7b97544c78dfed1c4dc73d6271b40ab
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-02-03 11:09:18 +0530

    Import done in Main.py, without fixing issues of GUI.py

--- Main.py
+++ Main.py
@@ -10,4 +10,15 @@ from Frontend.GUI import (
 )
 
 from Backend.Model import FirstLayerDMM
-from Backend.RealTimeSearchEngine import RealTimeSearchEngine
+from Backend.RealTimeSearchEngine import RealtimeSearchEngine
+from Backend. Automation import Automation
+from Backend.SpeechToText import SpeechRecognition
+from Backend.Chatbot import ChatBot
+from Backend.TTS import TextToSpeech
+from dotenv import load_dotenv
+from asyncio import run
+from time import sleep
+import subprocess
+import threading
+import json
+import os
