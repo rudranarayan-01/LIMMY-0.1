3466afc74b6c7428b81273002ed033aec8c7552f
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-02-03 20:17:44 +0530

    Main.py Updated

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from Frontend.GUI import (
     ShowTextToScreen,
     TempDirectoryPath,
     AnswerModifier,
+    QueryModifier,
     GetAssistantStatus,
     GetMicrophoneStatus
 )
@@ -96,4 +97,89 @@ def MainExecution():
     G = any([i for i in Decision if i.startswith("general")])
     R = any([i for i in Decision if i.startswith("realtime")])
     
+    Merged_query = "and".join(
+        [" ".join(i.split()[1: ]) for i in Decision if i.startswith("general") or i.startswith("realtime")]
+    )
     
+    for queries in Decision:
+        if "generate" in queries:
+            ImageGenerationQuery = queries.split("generate")[1].strip()
+            ImageExecution = True
+        
+    for queries in Decision:
+        if TaskExecution == False:
+            if any(queries.startswith(func) for func in Functions):
+                run(Automation(list(Decision)))
+                TaskExecution = True
+                
+    if ImageExecution == True:
+        with open(r"Frontend\Files\ImageGeneration.data", "w") as file:
+            file.write(f"{ImageGenerationQuery}, True")
+                
+                   
+        try:
+            p1 = subprocess.Popen(['python', r"Backend\ImageGeneration.py"],stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin= subprocess.PIPE, shell=False)
+            subprocess.append(p1)
+        except Exception as e:
+            print(f"Error starting ImageGeneration.py{e}")
+            
+    if G and R or R:
+        SetAssistantStatus("Searching....")
+        Answer = RealtimeSearchEngine(QueryModifier(Merged_query))
+        ShowTextToScreen(f"{AssistantName}:{Answer}")
+        SetAssistantStatus("Answering....")
+        TextToSpeech(Answer)
+        return True
+            
+    else :
+        for Queries in Decision:
+            if "general" in Queries:
+                SetAssistantStatus("Thinking....")
+                QueryFinal = Queries.replace("general ", "")
+                Answer = ChatBot(QueryModifier(QueryFinal))
+                ShowTextToScreen(f"{AssistantName}:{Answer}")
+                SetAssistantStatus("Answering....")
+                TextToSpeech(Answer)
+                return True
+            elif "realtime" in Queries:
+                SetAssistantStatus("Searching....")
+                QueryFinal = Queries.replace("realtime ", "")
+                Answer = ChatBot(QueryModifier(QueryFinal))
+                ShowTextToScreen(f"{AssistantName}:{Answer}")
+                SetAssistantStatus("Answering....")
+                TextToSpeech(Answer)
+                return True
+            elif "exit" in Queries:
+                QueryFinal = "Okay, Bye !"
+                Answer = ChatBot(QueryModifier(QueryFinal))
+                ShowTextToScreen = (f"{AssistantName}: {Answer}")
+                SetAssistantStatus("Answering....")
+                TextToSpeech(Answer)
+                SetAssistantStatus("Answering...")
+                os._exit(1)
+                
+def FirstThread():
+    
+    while True:
+        CurrentStatus = GetMicrophoneStatus()
+        
+        if CurrentStatus == "True":
+            MainExecution()
+            
+        else:
+            AIstatus = GetAssistantStatus()
+            
+            if "Available" in AIstatus:
+                sleep(0.1) 
+            else:
+                SetAssistantStatus("Available...")
+                
+def SecondThread():
+    GraphicalUserInterface()
+    
+    
+if __name__ == "__main__":
+    thread2 = threading.Thread(target=FirstThread, daemon=True)
+    thread2.start()
+    SecondThread()
+               
